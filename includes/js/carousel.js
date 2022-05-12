a13076db04efb68a86b52032ac0f523dcd3b8032
Ruben <0183263@student.roc-nijmegen.nl>
2022-05-12 14:01:32 +0200

    jafgqwfnks,g;h

---
+++ includes/js/carousel.js
@@ -0,0 +1,11 @@
+const carouselSlide = document.querySelector('.carousel_slide');
+const carouselImages = document.querySelectorALL('.carousel_slide img');
+
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+let counter = 1;
+const size = carouselImages[1].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
