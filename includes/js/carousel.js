65a27651363b62868b1630cc28c5c6592db12491
Ruben <0183263@student.roc-nijmegen.nl>
2022-05-19 10:54:34 +0200

    update carousel

--- includes/js/carousel.js
+++ includes/js/carousel.js
@@ -11,14 +11,14 @@ carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 nextBtn.addEventListener('click', ()=> {
     if (counter >= carouselImages.length -1) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    carouselSlide.style.transition = "transform 0.7s ease-in-out";
     counter++;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
 
 prevBtn.addEventListener('click', ()=> {
     if (counter <= 0) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    carouselSlide.style.transition = "transform 0.7s ease-in-out";
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
