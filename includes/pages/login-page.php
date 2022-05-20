48a71a5ee181c97c1446d4807b90b03fef3f2960
Ruben <0183263@student.roc-nijmegen.nl>
2022-05-20 09:11:49 +0200

    h

--- includes/pages/login-page.php
+++ includes/pages/login-page.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="../css/style.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
@@ -20,16 +20,32 @@
   <body>
     <main>
       <div class="login">
-            <H1>Inloggen</H1>
-            <form class="form_inlog" action="includes/login.php" method="POST">
-                <div class="inlog_style">
-                    <p>Username</p>
-                    <input type="text" name="naam" placeholder="naam">
-                    <p>Password</p>
-                    <input type="text" name="wachtwoord" placeholder="wachtwoord">
+            <form class="login_form" action="">
+                <h1>Waar wil je heen?</h1>
+                <div class="elem-group">
+                    <label for="name">Vanaf</label>
+                    <input type="text" id="name" name="department" placeholder="Vertrekluchthaven" pattern=[A-Z\sa-z]{3,20} required>
                 </div>
-                <button class="inlog_button" name="inlogSubmit">Login</button>
-            </from>
+                <div class="elem-group">
+                    <label for="name">Naar</label>
+                    <input type="text" id="name" name="arival" placeholder="Bestemming" required>
+                </div>
+                <div class="elem-group-inlined">
+                    <div class="date_selection">
+                        <label for="meeting-time">Vertrek op</label>
+                        <input type="datetime-local" id="date_selection" name="meeting-time" value="" min="2022-04-07T00:00" max="2023-01-01T00:00"></label>
+                    </div>
+                    <div class="date_selection">
+                        <label for="meeting-time">Vertrek op</label>
+                        <input type="datetime-local" id="date_selection" name="meeting-time" value="" min="2022-04-07T00:00" max="2023-01-01T00:00"></label>
+                    </div>
+                </div>
+                <div class="elem-group">
+                    <label for="adult">Wie gaan er mee?</label>
+                    <input type="number" id="adult" name="total_adults" placeholder="1 Volwassene" min="1" max="20" required>
+                </div>
+                <button type="submit">Zoeken</button>
+            </form>
       </div>
     </main>
     <script src="https://kit.fontawesome.com/460a8e1fd0.js" crossorigin="anonymous"></script>
