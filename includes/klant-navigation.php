99ee6a77446c20c329e34178aa0e574572655245
Ruben <0183263@student.roc-nijmegen.nl>
2022-06-16 20:15:13 +0200

    klant home pages

---
+++ includes/klant-navigation.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <div class="nav_items primary">
+            <ul>
+                <li><a href="../../index.php"><i class="fa-solid fa-house-chimney"></i></a></li>
+                <li><a href="">Plan en boek</a></li>
+                <li><a href="">Beheer je boeking</a></li>
+                <li><a href="">Service</a></li>
+            </ul>
+        </div>
+        <div class="nav_items secondary">
+            <ul>
+                <li><a href="">Bestemmingen</a></li>
+                <li><a href="overons.php">Over ons</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="review.php">Reviews</a></li>
+                <li><a>|</a></li>
+                <li><a href="../../includes/logout.php">Uitloggen</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
