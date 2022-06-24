a1eaed17c380eb041a8ecf49ad8a7aa8929b88ae
BigDickMilan <milantheunissen2003@gmail.com>
2022-06-24 11:03:04 +0200

    Update navigation.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -12,20 +12,20 @@ if (isset($_SESSION['ID'])) { ?>
         <nav>
             <div class="nav_items primary">
                 <ul>
-                    <li><a href="index.php"><i class="fa-solid fa-house-chimney"></i></a></li>
+                    <li><a href="../index.php"><i class="fa-solid fa-house-chimney"></i></a></li>
                     <li><a href="includes/login.php">Boekingen</a></li>
-                    <li><a href="contact.php">Service</a></li>
+                    <li><a href="../contact.php">Service</a></li>
                 </ul>
             </div>
             <div class="nav_items secondary">
                 <ul>
-                    <li><a href="bestemmingen.php">Bestemmingen</a></li>
-                    <li><a href="overons.php">Over ons</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                    <li><a href="review.php">Reviews</a></li>
+                    <li><a href="../bestemmingen.php">Bestemmingen</a></li>
+                    <li><a href="../overons.php">Over ons</a></li>
+                    <li><a href="../contact.php">Contact</a></li>
+                    <li><a href="../review.php">Reviews</a></li>
                     <li><a>|</a></li>
-                    <li><a href="pages/login-page.php">Inloggen</a></li>
-                    <li><a href="pages/register-page.php">Aanmelden</a></li>
+                    <li><a href="../pages/login-page.php">Inloggen</a></li>
+                    <li><a href="../pages/register-page.php">Aanmelden</a></li>
                 </ul>
             </div>
         </nav>
@@ -46,7 +46,7 @@ if (isset($_SESSION['ID'])) { ?>
         <div class="nav_items primary">
             <ul>
                 <li><a href="../../index.php"><i class="fa-solid fa-house-chimney"></i></a></li>
-                <li><a href="boekingen.php">Boekingen</a></li>
+                <li><a href="../boekingen.php">Boekingen</a></li>
                 <li><a href="../../contact.php">Service</a></li>
             </ul>
         </div>
