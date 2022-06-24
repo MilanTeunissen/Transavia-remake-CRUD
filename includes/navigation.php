0b4719be445e51bd34950ef4e4eaec38a376efb0
BigDickMilan <milantheunissen2003@gmail.com>
2022-06-24 10:04:04 +0200

    search-bar

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,5 +1,5 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) { ?>
+if (isset($_SESSION['ID'])) { ?>
         <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -32,7 +32,7 @@ if (session_status() === PHP_SESSION_NONE) { ?>
     </body>
     </html>
 <?php    
-} elseif (session_status() === PHP_SESSION_ACTIVE){ ?>
+} else { ?>
     <!DOCTYPE html>
 <html lang="en">
 <head>
